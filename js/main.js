1b6feea141534c013572d74b9817139b82c98a11
abdennour <homer.rider069@gmail.com>
2019-02-18 09:13:51 +0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,52 @@
 
+$( document ).ready(function() {
+
+	function _(element) {
+	    var element = document.querySelector(element);
+	    return element;
+	}
+
+	var ArrowLeft    = $('#SliderWrapper #ArrowLeft');
+	var ArrowRight   = $('#SliderWrapper #ArrowRight');
+	var Slides       = $('#Slides');
+	var CurrentSlide = 0;
+	var SlideWidth   = Slides.find('.slide').width();
+	var SlidesCount  = Slides.find('.slide').length;
+	var TotalSlidesWidth = (SlideWidth * SlidesCount) - SlideWidth;
+	var SlideSpeed   = 200;
+	var SlideTime    = 10000;
+	// Events
+	ArrowRight.on('click', priorSlide);
+	ArrowLeft.on('click', nextSlide);
+
+	// Functions
+	function nextSlide() {
+		Slides.animate({
+			marginLeft: '-=' +SlideWidth+'px'	
+		}, SlideSpeed, function() {
+			CurrentSlide++;
+			if ( CurrentSlide === SlidesCount ) {
+				Slides.css('margin-left', '0px');
+				CurrentSlide = 0;	
+			}	
+		});
+	}
+
+	function priorSlide() {
+		if ( CurrentSlide !== 0 ) {
+			Slides.animate({
+				marginLeft: '+=' +SlideWidth+ 'px'	
+			}, SlideSpeed, function() {
+				CurrentSlide--;
+				if ( CurrentSlide === 0 ) {
+					Slides.css('margin-left', -TotalSlidesWidth+'px');
+					CurrentSlide = SlidesCount;	
+				}	
+			});
+		}
+	}
+
+
+	
+
+});
